d5ab15139e257d203ae0de33180f92f430c46b6b
so7033300-sketch <so7033300@gmail.com>
2026-07-09 18:06:35 +0500

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const RENDER_BACKEND_URL = "https://onrender.com";
+const RENDER_BACKEND_URL = "https://muxtasham-jgqv.onrender.com";
 // 2. MA'LUMOTLARNI RENDER SERVERIGA VA POSTGRESQL BAZASIGA SAQLASH
 async function uploadLocalDataToBackend() {
     console.log("Render pullik serveriga zaxira nusxa yuklanmoqda...");
