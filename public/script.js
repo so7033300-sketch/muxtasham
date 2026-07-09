510b63b7eca3620feba32811477ecdfeee274cfc
so7033300-sketch <so7033300@gmail.com>
2026-07-10 00:34:06 +0500

    Update backend URL and maintain functionality

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const RENDER_BACKEND_URL = "https://onrender.com";
+const RENDER_BACKEND_URL = "https://muxtasham-jgqv.onrender.com";
 
 document.addEventListener("DOMContentLoaded", () => {
     renderTeachers();
